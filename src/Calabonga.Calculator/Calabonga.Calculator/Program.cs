3154324c577119b0a7e4f933b0048f431d1610e2
Calabonga <dev@calabonga.net>
2021-11-05 16:40:19 +0700

    Small reafactoring for method GetNumber

--- src/Calabonga.Calculator/Calabonga.Calculator/Program.cs
+++ src/Calabonga.Calculator/Calabonga.Calculator/Program.cs
@@ -12,21 +12,11 @@ namespace Calabonga.Calculator
             // Getting first number
             Console.WriteLine("Enter first number (float)");
             var number1 = GetNumber();
-            if (number1 is null)
-            {
-                Console.WriteLine("Wrong number. Good bye!");
-                return;
-            }
-
+            
             // Getting second number
             Console.WriteLine("Enter second number (float)");
             var number2 = GetNumber();
-            if (number2 is null)
-            {
-                Console.WriteLine("Wrong number. Good bye!");
-                return;
-            }
-
+            
             // Getting Operand
             var operand = GetOperand();
             if (operand == OperandType.None)
@@ -36,12 +26,11 @@ namespace Calabonga.Calculator
             }
 
             // Calculation 
-            var result = Calculate(number1.Value, number2.Value, operand);
+            var result = Calculate(number1, number2, operand);
 
             // Showing result
             Console.WriteLine(result);
         }
-
 
         /// <summary>
         /// Returns calculation result
@@ -98,14 +87,21 @@ namespace Calabonga.Calculator
         /// Returns parsed number
         /// </summary>
         /// <returns></returns>
-        private static float? GetNumber()
+        private static float GetNumber()
         {
-            var numberString = Console.ReadLine();
-            var isOkNumber1 = float.TryParse(numberString, out var number);
-            if (!isOkNumber1)
+            float number;
+            bool isInputValid;
+            do
             {
-                return null;
-            }
+                var numberString = Console.ReadLine();
+                var numberParsed = float.TryParse(numberString, out number);
+                isInputValid = numberParsed;
+                if (!numberParsed)
+                {
+                    Console.WriteLine("Wrong number, please try again enter a valid float number:");
+                }
+            } while (!isInputValid);
+
             return number;
         }
     }
